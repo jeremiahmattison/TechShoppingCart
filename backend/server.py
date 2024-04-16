92f2bbfdfe011159d52280a532a476d7639ddabe
jeremiahmattison <mattisonjeremiah@gmail.com>
2024-04-15 20:31:52 -0400

    server connects

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify
+import products_dao
+from sql_connection import get_sql_connection
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "Hello, how are you?"
+connection = get_sql_connection()
+
+@app.route('/getProducts', methods=['GET'])
+def get_products():
+    products = products_dao.get_all_products(connection)
+    response = jsonify(products) 
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server for Online Shopping Store Management System")
